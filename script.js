061a8d6127856058ba1f88d0d4c8ec384cbbec72
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-10 22:53:52 -0400

    Primeira requisição GET

--- script.js
+++ script.js
@@ -7,5 +7,39 @@
     Tabela para trazer os dados de todos os usuários atualizados:
         Botão para atualizar e buscar os dados atualizados
         Botão para excluir usuário dentro da tabela
-        
+
 */
+
+const sendForm = async (params) => {
+    return 1;
+}
+
+document.getElementById('sendButton').addEventListener('click', () => {
+    sendForm(/* Adicionar os parametros para chamada da função */)
+})
+
+
+
+const getUsers = async () => {
+    try {
+        // Fazendo a requisição GET para o endpoint da API
+        const response = await fetch('http://localhost:3000/usuarios')
+
+        // Verificando se a requisição foi bem-sucedida
+        if (!response.ok) {
+            throw new Error('Erro ao buscar os dados dos usuários')
+        }
+
+        // Obtendo os dados da resposta em formato JSON
+        const users = await response.json()
+
+        // Exibindo os dados no console (você pode manipular os dados como quiser)
+        console.log(users)
+
+    } catch (error) {
+        // Caso ocorra um erro
+        console.error('Erro ao buscar usuários:', error);
+    }
+}
+
+getUsers()
