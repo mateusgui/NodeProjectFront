c8a432b008b0653ec2b2dca2a190215a75604ca8
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-10 21:50:28 -0400

    Layout Finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+
+    Enviar os dados para o banco de dados
+        SUCESSO: Retornar os dados do usuário criado
+        ERRO: Retornar mensagem de erro
+
+    Tabela para trazer os dados de todos os usuários atualizados:
+        Botão para atualizar e buscar os dados atualizados
+        Botão para excluir usuário dentro da tabela
+        
+*/
